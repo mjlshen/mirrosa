78053ec6dd1c09d26e9d0ed79cb813ee30ec5286
boranx <boran.seref@gmail.com>
2022-12-06 19:07:03 +0100

    feat: add instance validations

--- pkg/mirrosa/aws.go
+++ pkg/mirrosa/aws.go
@@ -2,11 +2,13 @@ package mirrosa
 
 import (
 	"context"
+
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/aws/aws-sdk-go-v2/service/route53"
 )
 
 type Ec2AwsApi interface {
+	DescribeInstances(ctx context.Context, params *ec2.DescribeInstancesInput, optFns ...func(*ec2.Options)) (*ec2.DescribeInstancesOutput, error)
 	ec2.DescribeSecurityGroupsAPIClient
 	ec2.DescribeSecurityGroupRulesAPIClient
 	ec2.DescribeSubnetsAPIClient
